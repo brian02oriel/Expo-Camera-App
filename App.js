0d95dd6d323a391498733fe160bf7575a405bbfc
Brian Nieto <brian.nieto1@utp.ac.pa>
2021-03-28 14:41:23 -0500

    Basic functionalities

--- App.js
+++ App.js
@@ -1,21 +1,264 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Switch, TouchableOpacity, Image } from "react-native";
+import * as Permissions from 'expo-permissions'
+import { Camera } from 'expo-camera'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+initialState = {
+  switchValue: false,
+  hasCameraPermission: null,
+  type: Camera.Constants.Type.back,
+  imageuri: "",
+  url: ""
 }
+export default class App extends React.Component {
+  state = {
+   ...initialState
+  };
 
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === "granted" });
+  }
+
+  cameraChange = () => {
+    this.setState({
+      imageuri: "",
+      url: "",
+      type:
+        this.state.type === Camera.Constants.Type.back
+          ? Camera.Constants.Type.front
+          : Camera.Constants.Type.back
+    });
+  };
+
+  snap = async () => {
+    if (this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      if (photo) {
+        console.log("photo: ", photo)
+        this.setState({ imageuri: photo.uri });
+      }
+    }
+  };
+
+  clearData = async () => {
+    this.setState({ imageuri: "" });
+  }
+
+
+  upload = () => {
+    const file = {
+      uri: this.state.imageuri,
+      name: `${new Date().getTime()}.jpg`,
+      type: "image/jpeg"
+    };
+    const options = {
+      keyPrefix: "ts/",
+      bucket: "..name..",
+      region: "eu-west-1",
+      accessKey: "..acesskey..",
+      secretKey: "..secretkey..",
+      successActionStatus: 201
+    };
+    return RNS3.put(file, options)
+      .then(response => {
+        if (response.status !== 201)
+          throw new Error("Failed to upload image to S3");
+        else {
+          console.log(
+            "Successfully uploaded image to s3. s3 bucket url: ",
+            response.body.postResponse.location
+          );
+          this.setState({
+            url: response.body.postResponse.location,
+            switchValue: false
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { hasCameraPermission } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return (
+        <View>
+          <Text>No access to camera</Text>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <View style={styles.switchview}>
+            <Text>Show camera</Text>
+            <Switch
+              onValueChange={value => {
+                this.setState({ switchValue: value });
+              }}
+              value={this.state.switchValue}
+              style={styles.switch}
+            />
+          </View>
+          {this.state.switchValue ? (
+            <View style={styles.cameraview}>
+              {this.state.imageuri != "" ? (
+                <Image
+                  source={{
+                    uri: this.state.imageuri
+                  }}
+                  style={styles.uploadedImage}
+                  resizeMode="contain"
+                />
+              ) : (
+                <Camera
+                  style={styles.camera}
+                  type={this.state.type}
+                  ref={ref => {
+                    this.camera = ref;
+                  }}
+                >
+                  <View style={styles.camerabuttonview}>
+                    <TouchableOpacity
+                      style={styles.cameraButtons}
+                      onPress={this.cameraChange}
+                    >
+                      <Text
+                        style={{
+                          fontSize: 18,
+                          marginBottom: 10,
+                          color: "white"
+                        }}
+                      >
+                        Flip
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </Camera>
+              )}
+            </View>
+          ) : (
+            <View style={styles.cameraview}>
+              {this.state.url != "" ? (
+                <Text>Uploaded url : {this.state.url}</Text>
+              ) : null}
+              <Text>Camera off</Text>
+            </View>
+          )}
+          {this.state.switchValue ? (
+            <View style={styles.buttonsView}>
+              {this.state.imageuri == "" ? (
+                <View style={styles.captureButtonView}>
+                  <TouchableOpacity
+                    style={styles.cameraButtons}
+                    onPress={this.snap}
+                  >
+                    <Text
+                      style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                    >
+                      Capture
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              ) : 
+              <View style={styles.postCaptureView}>
+                <View style={styles.captureButtonView}>
+                  <TouchableOpacity
+                    style={styles.cameraButtons}
+                    onPress={this.clearData}
+                  >
+                    <Text
+                      style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                    >
+                      Take Another
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.captureButtonView}>
+                  <TouchableOpacity
+                    style={styles.cameraButtons}
+                  >
+                    <Text
+                      style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                    >
+                      Upload
+                    </Text>
+                  </TouchableOpacity>
+                </View> 
+              </View>
+              }
+
+            </View>
+          ) : null}
+        </View>
+      );
+    }
+  }
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1dd1a1",
+    alignItems: "center",
+    justifyContent: "flex-start"
   },
+  switchview: {
+    marginTop: 50,
+    backgroundColor: "white",
+    padding: 10,
+    alignItems: "center",
+    borderRadius: 5,
+    marginBottom: 5
+  },
+  switch: {
+    padding: 5
+  },
+  cameraview: {
+    height: 400,
+    width: 300,
+    backgroundColor: "white",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  camera: {
+    height: "95%",
+    width: "95%",
+    backgroundColor: "white",
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  camerabuttonview: {
+    height: "100%",
+    backgroundColor: "transparent"
+  },
+  cameraButtons: {
+    borderColor: "#fff",
+    borderWidth: 2,
+    padding: 10,
+    borderRadius: 5,
+    margin: 5
+  },
+  captureButtonView: {
+    height: 200
+  },
+  buttonsView: {
+    height: 200,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  postCaptureView:{
+    flexDirection: "row",
+    justifyContent: "center"
+  },
+  uploadedImage: {
+    height: "90%",
+    width: "90%",
+    padding: 10
+  }
 });
